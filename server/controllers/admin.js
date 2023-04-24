d0d8e512f7e045b7e7563d6a6e6be942e1a98cf4
Mathew Reforma <reformamathew@protonmail.com>
2023-04-25 00:20:36 +0800

    Correct order of recent violations data

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -3,13 +3,13 @@ const db = require('../db/connect.js');
 let sql;
 
 const getRecentViolations = (req, res) => {
-  sql = 'SELECT * FROM Violations ORDER BY date(Timestamp) DESC LIMIT 50';
+  sql = 'SELECT Violations.Timestamp, Violations.StudentId, Students.FirstName, Students.LastName, Violations.Violation, Violations.Timestamp, Students.Course, Students.Year, Violations.Guard FROM Violations INNER JOIN Students ON Violations.StudentId = Students.StudentId ORDER BY datetime(Timestamp) DESC LIMIT 50';
   db.all(sql, [], (err, rows) => {
-    if (err) return console.err(err);
+    if (err) return console.error(err);
 
     const newRows = rows.map((row) => {
-      const { Violation: violation, Guard: guard, Timestamp: timestamp, StudentId: studentid } = row;
-      return { timestamp, studentid, violation, guard };
+      const { Violation: violation, Guard: guard, Timestamp: timestamp, StudentId: studentId } = row;
+      return { timestamp, studentId, violation, guard };
     })
 
     res.status(200).send({ success: true, data: newRows });
