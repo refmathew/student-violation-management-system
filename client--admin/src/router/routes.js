31365c3563ebe27fc93521698325c0d540041091
Mathew Reforma <reformamathew@protonmail.com>
2023-05-03 10:17:24 +0800

    Add route name

--- client--admin/src/router/routes.js
+++ client--admin/src/router/routes.js
@@ -11,13 +11,13 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: Index, meta: { title: 'Home' } },
-      { path: 'find', component: Find, meta: { title: 'Find student' } },
-      { path: 'filter', component: Filter, meta: { title: 'Filter violations' } },
-      { path: 'stats/violation', component: Violation, meta: { title: 'Violation' } },
-      { path: 'stats/time-and-date', component: Time, meta: { title: 'Time & Date' } },
-      { path: 'stats/course-and-year', component: Course, meta: { title: 'Course & Year' } },
-      { path: 'stats/guard', component: Guard, meta: { title: 'Guard' } }
+      { path: '', name: 'indexPage', component: Index, meta: { title: 'Home' } },
+      { path: 'find', name: 'findPage', component: Find, meta: { title: 'Find student' } },
+      { path: 'filter', name: 'filterPage', component: Filter, meta: { title: 'Filter violations' } },
+      { path: 'stats/violation', name: 'statsViolationPage', component: Violation, meta: { title: 'Violation' } },
+      { path: 'stats/time-and-date', name: 'statsTimeAndDatePage', component: Time, meta: { title: 'Time & Date' } },
+      { path: 'stats/course-and-year', name: 'statsCourseAndYearPage', component: Course, meta: { title: 'Course & Year' } },
+      { path: 'stats/guard', name: 'statsGuardPage', component: Guard, meta: { title: 'Guard' } }
     ]
   },
 
