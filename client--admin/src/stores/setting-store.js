0a60a4e8a73101234d7988a0c8b568d49b1159ed
Mathew Reforma <reformamathew@protonmail.com>
2023-05-13 11:51:32 +0800

    Change default scope of data to year, upon app init

--- client--admin/src/stores/setting-store.js
+++ client--admin/src/stores/setting-store.js
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 export const useSettingStore = defineStore('setting', {
   state: () => {
     return {
-      scope: 'week',
+      scope: 'year',
       displayMajor: 1,
     }
   },
