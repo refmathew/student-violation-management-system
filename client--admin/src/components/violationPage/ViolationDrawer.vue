39bfd6059c0d0dae40cd4590580c23050c5518f3
Mathew Reforma <reformamathew@protonmail.com>
2023-05-13 11:50:34 +0800

    Fix color of violation not matching legend in violation stats page

---
+++ client--admin/src/components/violationPage/ViolationDrawer.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="setting-drawer" ref="settingDrawer">
+    <div class="setting-drawer__handle">
+      <div />
+      <div />
+    </div>
+    <div class="setting-drawer__main">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      settingDrawerHeight: undefined,
+      settingDrawerHandleHeight: undefined,
+    };
+  },
+  mounted() {
+    // (() => {
+    // this.settingDrawerWidth = this.$refs.settingDrawer.clientWidth;
+    // this.settingDrawerHandleWidth = this.$refs.settingDrawer.firstChild.clientWidth;
+    // this.$refs.settingDrawer.style.bottom = `-${this.settingDrawerWidth - this.settingDrawerHandleWidth - 8}px`;
+
+    // this.$refs.settingDrawer.childNodes.forEach((child) => {
+    // child.addEventListener("mouseover", () => {
+    // this.$refs.settingDrawer.style.bottom = 0;
+    // });
+    // });
+
+    // this.$refs.settingDrawer.childNodes.forEach((child) => {
+    // child.addEventListener("mouseleave", () => {
+    // this.$refs.settingDrawer.style.bottom = `-${this.settingDrawerWidth - this.settingDrawerHandleWidth - 8}px`;
+    // });
+    // });
+    // })();
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.setting-drawer {
+  position: fixed;
+  height: 80%;
+  right: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  transition: bottom 400ms ease;
+
+  &__handle {
+    display: flex;
+    flex-direction: column;
+    gap: .2rem;
+    padding: .8rem 1.6rem;
+    border-radius: 3.2rem 3.2rem 0 0;
+    background-color: $secondary-1;
+    box-shadow: 0 .4rem .8rem .2rem rgba($primary-2, .32);
+
+    div {
+      width: 3.2rem;
+      height: .2rem;
+      background-color: $primary-2;
+      border-radius: .1rem;
+    }
+  }
+
+  &__main {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    gap: 12.8rem;
+    min-height: 6.4rem;
+    padding: 1.6rem 25.6rem 1.2rem;
+    background-color: $secondary-1;
+    border-radius: 3.2rem 3.2rem 0 0;
+    box-shadow: 0 .4rem .8rem .2rem rgba($primary-2, .32);
+  }
+
+}
+</style>
--- client--admin/src/pages/stats/ViolationPage.vue
+++ client--admin/src/pages/stats/ViolationPage.vue
@@ -3,14 +3,17 @@
     <div class="page--stats--violation__chart-title">
       {{ `${this.getLevel === 1 ? 'MAJOR' : 'MINOR'} Violations this ${this.getScope.toUpperCase()}` }}
     </div>
-    <div class="page--stats--violation__legends">
-      <button class="page--stats--violation__legend" v-for="( violation, index ) in getViolations" :key="index">
-        <span class="page--stats--violation__legend-number">[{{ violation.violationNumber }}] </span>
-        <span class="page--stats--violation__legend-name">{{ violation.violation }}</span>
-      </button>
-    </div>
-    <div class="page--stats--violation__chart-container">
-      <Pie class="page--stats--violation__chart" id="my-chart-id" :options="getChartOptions" :data="getChartData" />
+    <div class="page--stats--violation__main">
+      <div class="page--stats--violation__chart-container">
+        <Pie class="page--stats--violation__chart" id="my-chart-id" :options="getChartOptions" :data="getChartData" />
+      </div>
+      <table class="page--stats--violation__legends">
+        <tr class="page--stats--violation__legend" :data-index="index % 4" v-for="( violation, index ) in getViolations"
+          :key="index">
+          <td class="page--stats--violation__legend-number">{{ violation.violationNumber }} </td>
+          <td>{{ violation.violation }}</td>
+        </tr>
+      </table>
     </div>
   </q-page>
 </template>
@@ -18,18 +21,19 @@
 <script>
 import { Pie } from 'vue-chartjs';
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, RadialLinearScale, ArcElement, Colors, plugins } from 'chart.js';
-import autocolors from 'chartjs-plugin-autocolors';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { useViolationStore } from 'src/stores/violation-store';
 import { useSettingStore } from 'src/stores/setting-store';
 
-ChartJS.register(Title, Tooltip, Legend, RadialLinearScale, ArcElement, autocolors);
+ChartJS.register(Title, Tooltip, Legend, RadialLinearScale, ArcElement);
 
 export default {
   data() {
     return {
       violationStore: useViolationStore(),
       settingStore: useSettingStore(),
+      backgroundColors: ['#F86A9D52', '#6A79FF52', '#08AF4B52', '#F88C6A52',],
+      borderColors: ['#F86A9DCC', '#6A79FFCC', '#08AF4BCC', '#F88C6ACC',],
     }
   },
   computed: {
@@ -50,7 +54,24 @@ export default {
           },
           colors: true,
           datalabels: {
-            color: '#120e41',
+            padding: {
+              top: 4,
+              bottom: 4,
+              right: 16,
+              left: 16,
+            },
+            anchor: 'end',
+            align: 'start',
+            offset: -4,
+            backgroundColor: this.borderColors,
+            borderColor: this.backgroundColors,
+            borderRadius: 8,
+            borderWidth: 2,
+            font: {
+              size: 12,
+              weight: 600,
+            },
+            color: '#f2f2fd',
             formatter: (value, context) => {
               var regExp = /\[(.*?)\]/;
               const label = context.chart.data.labels[context.dataIndex]
@@ -58,7 +79,7 @@ export default {
             }
           },
           legend: {
-            display: true,
+            display: false,
             fullsize: true,
             align: 'left',
             position: 'top',
@@ -110,40 +131,25 @@ export default {
       }
 
       return this.violationStore.stats.violations[this.getScope]
+        .filter(violation => violation.violationIsMajor === this.getLevel)
     },
     getChartData() {
       return {
         labels: this.getLabels,
         datasets: [{
           label: 'Violation count',
-          borderWidth: 8,
+          // spacing: 2,
+          offset: 32,
+          borderWidth: 2,
           borderJoinStyle: 'round',
-          // backgroundColor: [
-          // '#120E4129',
-          // '#6F6AF87A',
-          // '#8B86BD7A',
-          // ],
-          borderColor: '#f2f2fd',
+          backgroundColor: this.backgroundColors,
+          borderColor: this.borderColors,
+          hoverBackgroundColor: this.borderColors,
+          hoverBorderColor: this.backgroundColors,
           data: this.getData
         }]
       }
     },
-  },
-  methods: {
-    poolColors(a) {
-      let pool = [];
-      for (i = 0; i < a; i++) {
-        pool.push(dynamicColors());
-      }
-      return pool;
-    },
-
-    dynamicColors() {
-      var r = Math.floor(Math.random() * 255);
-      var g = Math.floor(Math.random() * 255);
-      var b = Math.floor(Math.random() * 255);
-      return "rgb(" + r + "," + g + "," + b + ")";
-    }
   },
   components: {
     Pie,
@@ -162,6 +168,16 @@ export default {
   align-items: center;
   gap: 1.6rem;
 
+  &__main {
+    display: flex;
+    gap: 1.6rem;
+    align-items: center;
+
+    &>* {
+      flex: 1;
+    }
+  }
+
   &__chart-title {
     font-size: 1.6rem;
     font-weight: 600;
@@ -177,6 +193,38 @@ export default {
   &__chart {
     width: 100% !important;
     height: 100% !important;
+  }
+
+  &__legends {
+    max-height: 54.3rem;
+    overflow-y: scroll;
+    border-radius: 3.2rem;
+    display: block;
+  }
+
+  &__legend {
+    color: #FFFFFF;
+    border-radius: 3.2rem;
+
+    &>* {
+      padding: 1.6rem;
+    }
+
+    &[data-index="0"] {
+      background-color: #F86A9DCC;
+    }
+
+    &[data-index="1"] {
+      background-color: #6A79FFCC;
+    }
+
+    &[data-index="2"] {
+      background-color: #08AF4BCC;
+    }
+
+    &[data-index="3"] {
+      background-color: #F88C6ACC;
+    }
   }
 }
 </style>
